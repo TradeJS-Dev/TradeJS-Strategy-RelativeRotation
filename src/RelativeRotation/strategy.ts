f89057850d7ac605859dea27198978e1bb3e2aa3
aleksnick <nikolaev@iprojects.ru>
2026-08-20 15:51:20 +0300

    feat: validate strategy configuration

--- src/RelativeRotation/strategy.ts
+++ src/RelativeRotation/strategy.ts
@@ -1,11 +1,16 @@
-import type { StrategyRegistryEntry } from "@tradejs/types";
+import { createStrategyConfigParser } from "@tradejs/strategy-kit/config";
+import type { ValidatedStrategyRegistryEntry } from "@tradejs/strategy-kit/config";
 import { RelativeRotationConfig, config as DEFAULT_CONFIG } from "./config";
 import { createRelativeRotationCore } from "./core";
 import { relativeRotationManifest } from "./manifest";
 
-export const RelativeRotationStrategyDefinition: StrategyRegistryEntry<RelativeRotationConfig> =
+export const RelativeRotationStrategyDefinition: ValidatedStrategyRegistryEntry<RelativeRotationConfig> =
   {
     defaults: DEFAULT_CONFIG,
+    parseConfig: createStrategyConfigParser({
+      strategyName: "RelativeRotation",
+      defaults: DEFAULT_CONFIG,
+    }),
     createCore: createRelativeRotationCore,
     manifest: relativeRotationManifest,
   };
